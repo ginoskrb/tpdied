104a21a2333669957f18f72b2b4b134b0c0b1355
Franco <programmingarmanasco@gmail.com>
2023-07-19 12:18:13 -0300

    filtrador de stock

--- tpiarmsca/src/main/java/gui/stock/StockSucursal.java
+++ tpiarmsca/src/main/java/gui/stock/StockSucursal.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import gui.sucursal.EditarSucursal;
 import gui.sucursal.PanelSucursal;
@@ -22,11 +24,14 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class StockSucursal extends JFrame {
 
 	private JPanel contentPane;
 	private JTable tablaStock;
+	private JTextField buscadorProducto;
 
 	/**
 	 * Launch the application.
@@ -53,7 +58,7 @@ public class StockSucursal extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 64, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setSize(700,500);
+		setSize(700,518);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		
@@ -61,7 +66,7 @@ public class StockSucursal extends JFrame {
 		contentPane.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 11, 664, 362);
+		scrollPane.setBounds(10, 44, 664, 362);
 		contentPane.add(scrollPane);
 		
 		tablaStock = new JTable();
@@ -74,7 +79,7 @@ public class StockSucursal extends JFrame {
 		botonAgregar.setForeground(new Color(0, 64, 128));
 		botonAgregar.setFont(new Font("Dialog", Font.BOLD, 15));
 		botonAgregar.setBackground(Color.WHITE);
-		botonAgregar.setBounds(110, 396, 99, 35);
+		botonAgregar.setBounds(111, 433, 99, 35);
 		botonAgregar.addActionListener(e->{
 			AgregarStock.main(null, this, suc);
 		});
@@ -84,7 +89,7 @@ public class StockSucursal extends JFrame {
 		botonEditar.setForeground(new Color(0, 64, 128));
 		botonEditar.setFont(new Font("Dialog", Font.BOLD, 15));
 		botonEditar.setBackground(Color.WHITE);
-		botonEditar.setBounds(288, 396, 99, 35);
+		botonEditar.setBounds(288, 433, 99, 35);
 		contentPane.add(botonEditar);
 		botonEditar.addActionListener(e->{
 			int filaSeleccionada = tablaStock.getSelectedRow();
@@ -101,7 +106,7 @@ public class StockSucursal extends JFrame {
 		botonEliminar.setForeground(new Color(0, 64, 128));
 		botonEliminar.setFont(new Font("Dialog", Font.BOLD, 15));
 		botonEliminar.setBackground(Color.WHITE);
-		botonEliminar.setBounds(469, 396, 99, 35);
+		botonEliminar.setBounds(471, 433, 99, 35);
 		botonEliminar.addActionListener( e-> {
 				int filaSeleccionada = tablaStock.getSelectedRow();
 				if (filaSeleccionada != -1) {
@@ -119,9 +124,38 @@ public class StockSucursal extends JFrame {
                 } 
             });
 		contentPane.add(botonEliminar);
+		
+		JLabel lblNewLabel = new JLabel("Nombre producto");
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		lblNewLabel.setBounds(182, 11, 132, 14);
+		contentPane.add(lblNewLabel);
+		
+		buscadorProducto = new JTextField();
+		buscadorProducto.setBounds(324, 10, 132, 20);
+		contentPane.add(buscadorProducto);
+		buscadorProducto.setColumns(10);
+		
+		buscadorProducto.getDocument().addDocumentListener(new DocumentListener() {
+			public void insertUpdate(DocumentEvent e) {
+				filterTablaStock(buscadorProducto.getText(),suc);
+			}
+			public void removeUpdate(DocumentEvent e) {
+				filterTablaStock(buscadorProducto.getText(),suc);
+			}
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+			}
+		});
+		
 	}
 	
 	public JTable getTablaStock() {
 		return tablaStock;
+	}
+	
+	private void filterTablaStock(String nombreProducto, int idSucursal) {
+		tablaStock.setModel(new StockController().filtrarTablaPorNombre(nombreProducto,idSucursal));
 	}
 }
